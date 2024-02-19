e496eef8302f359d72d62275864c2703c880715b
Maarij Waseem <112642915+maarijwaseem2@users.noreply.github.com>
2024-02-19 20:06:27 +0500

    Delete api directory

--- api/models/product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ProductSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: {type :String, required: true},
-    price: {type: Number, required: true}
-});
-
-module.exports = mongoose.model('Product',ProductSchema);
